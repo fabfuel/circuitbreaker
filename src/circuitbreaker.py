2c90feea52788852c72a0eed42a04c6fa61abfc6
Fabian Fülling <git@fabfuel.de>
2016-02-07 10:18:47 +0100

    Some cleanup and refactorings

--- src/circuitbreaker.py
+++ src/circuitbreaker.py
@@ -1,75 +1,130 @@
 from functools import wraps
 from datetime import datetime, timedelta
-from logging import getLogger
 
-logger = getLogger('circuitbreaker')
-
-STATE_OPEN = 'open'
-STATE_HALFOPEN = 'halfopen'
 STATE_CLOSED = 'closed'
+STATE_OPEN = 'open'
+STATE_HALF_OPEN = 'half_open'
 
 
 class CircuitBreaker(object):
-    def __init__(self, exception=Exception, failure_threshold=5, retry_timeout=30):
-        self._exception = exception
+    def __init__(self, expected_exception=Exception, failure_threshold=5, recover_timeout=30, name=None):
+        self._expected_exception = expected_exception
         self._failure_count = 0
         self._failure_threshold = failure_threshold
-        self._retry_timeout = retry_timeout
+        self._recover_timeout = recover_timeout
         self._state = STATE_CLOSED
         self._opened = datetime.utcnow()
+        self._name = name
 
     def __call__(self, wrapped):
+        """
+        Applies the circuit breaker decorator to a function
+        """
+        if self._name is None:
+            self._name = wrapped.__name__
+
         @wraps(wrapped)
         def wrapper(*args, **kwargs):
             return self.call(wrapped, *args, **kwargs)
+
         return wrapper
 
     def call(self, func, *args, **kwargs):
-        if self.is_open():
-            print('  \__ %s failures' % self._failure_count)
-            raise CircuitBreakerOpenError('CIRCUIT: open after %d failures until %s for another %s seconds' % (self._failure_threshold, self.open_until, self.open_remaining))
-
+        """
+        Calls the decorated function and applies the circuit breaker rules on success or failure
+        :param func: Decorated function
+        """
+        if not self.closed:
+            raise CircuitBreakerError(self)
         try:
             result = func(*args, **kwargs)
-        except self._exception as e:
-            self._failure_count += 1
-
-            if self._failure_count >= self._failure_threshold:
-                self.open(e)
-
+        except self._expected_exception:
+            self.on_failure()
             raise
         else:
-            self._state = STATE_CLOSED
-            self._failure_count = 0
+            self.on_success()
             return result
 
-        finally:
-            print('  \__ %s failures' % self._failure_count)
+    def on_success(self):
+        """
+        Close circuit after successful execution
+        """
+        self.close()
 
-    def is_open(self):
-        timeout = timedelta(seconds=self._retry_timeout)
-        if self._state == STATE_OPEN and timeout < (datetime.utcnow() - self._opened):
-            self._state = STATE_HALFOPEN
-            return False
+    def on_failure(self):
+        """
+        Count failure and open circuit, if threshold has been reached
+        """
+        self._failure_count += 1
+        if self._failure_count >= self._failure_threshold:
+            self.open()
 
-        return self._state == STATE_OPEN
-
-    def open(self, exception):
+    def open(self):
+        """
+        Open the circuit breaker
+        """
         self._state = STATE_OPEN
         self._opened = datetime.utcnow()
 
+    def close(self):
+        """
+        Close the circuit breaker
+        """
+        self._state = STATE_CLOSED
+        self._failure_count = 0
+
+    @property
+    def closed(self):
+        """
+        Check if state is CLOSED
+        Set state to HALF_OPEN and allow the next execution, if recovery timeout has been reached
+        """
+        if self._state == STATE_OPEN and self.open_remaining <= 0:
+            self._state = STATE_HALF_OPEN
+            return True
+
+        return self._state == STATE_CLOSED
 
     @property
     def open_until(self):
-        return self._opened + timedelta(seconds=self._retry_timeout)
+        """
+        The datetime, when the circuit breaker will try to recover
+        :return: datetime
+        """
+        return self._opened + timedelta(seconds=self._recover_timeout)
 
     @property
     def open_remaining(self):
+        """
+        Number of seconds (int) remaining, the circuit breaker stays in OPEN state
+        :return: int
+        """
         return (self.open_until - datetime.utcnow()).total_seconds()
+
+    @property
+    def failure_count(self):
+        return self._failure_count
+
+    @property
+    def name(self):
+        return self._name
 
 
 class CircuitBreakerError(Exception):
-    pass
+    def __init__(self, circuit_breaker, *args, **kwargs):
+        """
+        :param circuit_breaker:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        super().__init__(*args, **kwargs)
+        self._circuit_breaker = circuit_breaker
 
-class CircuitBreakerOpenError(CircuitBreakerError):
-    pass
+    def __str__(self, *args, **kwargs):
+        return 'CIRCUIT "%s" OPEN until %s (%d failures, %d sec remaining)' % (
+            self._circuit_breaker.name,
+            self._circuit_breaker.open_until,
+            self._circuit_breaker.failure_count,
+            round(self._circuit_breaker.open_remaining)
+        )
--- src/example.py
+++ src/example.py
@@ -1,22 +1,18 @@
 from time import sleep
-
 from src.circuitbreaker import CircuitBreaker
 
-breaker = CircuitBreaker(failure_threshold=1, retry_timeout=5)
 
-@breaker
-def callx(i):
-    if i in (2, 3, 6, 7, 10, 12, 15):
+@CircuitBreaker(failure_threshold=2, recover_timeout=3)
+def external_call(call_id):
+    if call_id in (2, 3, 6, 7, 10, 12, 15):
         raise ConnectionError('Connection refused')
-    print('## SUCCESS')
+    return 'SUCCESS'
 
-if __name__ == '__main__':
+for i in range(0, 20):
+    try:
+        print('CALL: %d' % i)
+        print(' ## %s ' % external_call(i))
+    except Exception as e:
+        print('  \__ %s ' % e)
 
-    for i in range(0, 20):
-        try:
-            print('CALL: %s' % i)
-            callx(i)
-        except Exception as e:
-            print('  \__ ' + str(e))
-
-        sleep(1)
+    sleep(0.5)
