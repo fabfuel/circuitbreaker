5cd048ff7df678b78d2d3c481ab1f545da7e23a9
Paulo Romeira <paulo@pauloromeira.com>
2023-04-10 18:59:05 -0300

    tests: Unify test_functional and test_functional_async modules

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,64 +1,32 @@
-from time import sleep
+import asyncio
+import time
 
-try:
-    from unittest.mock import Mock, patch
-except ImportError:
-    from mock import Mock, patch
-
-from pytest import raises
+import pytest
 
 from circuitbreaker import CircuitBreaker, CircuitBreakerError, \
     CircuitBreakerMonitor, STATE_CLOSED, STATE_HALF_OPEN, STATE_OPEN
 
 
-def pseudo_remote_call():
-    return True
+async def test_circuit_pass_through(sync_or_async, circuit_success):
+    assert await sync_or_async(circuit_success()) is True
 
 
-@CircuitBreaker()
-def circuit_success():
-    return pseudo_remote_call()
-
-
-@CircuitBreaker(failure_threshold=1, name="circuit_failure")
-def circuit_failure():
-    raise IOError()
-
-
-@CircuitBreaker(failure_threshold=1, name="circuit_generator_failure")
-def circuit_generator_failure():
-    pseudo_remote_call()
-    yield 1
-    raise IOError()
-
-
-@CircuitBreaker(failure_threshold=1, name="threshold_1")
-def circuit_threshold_1():
-    return pseudo_remote_call()
-
-
-@CircuitBreaker(failure_threshold=2, recovery_timeout=1, name="threshold_2")
-def circuit_threshold_2_timeout_1():
-    return pseudo_remote_call()
-
-
-@CircuitBreaker(failure_threshold=3, recovery_timeout=1, name="threshold_3")
-def circuit_threshold_3_timeout_1():
-    return pseudo_remote_call()
-
-
-def test_circuit_pass_through():
-    assert circuit_success() is True
-
-
-def test_circuitbreaker_monitor():
+@pytest.mark.usefixtures(
+    "circuit_success",
+    "circuit_failure",
+    "circuit_generator_failure",
+    "circuit_threshold_1",
+    "circuit_threshold_2_timeout_1",
+    "circuit_threshold_3_timeout_1",
+)
+async def test_circuitbreaker_monitor(sync_or_async, circuit_failure):
     assert CircuitBreakerMonitor.all_closed() is True
     assert len(list(CircuitBreakerMonitor.get_circuits())) == 6
     assert len(list(CircuitBreakerMonitor.get_closed())) == 6
     assert len(list(CircuitBreakerMonitor.get_open())) == 0
 
-    with raises(IOError):
-        circuit_failure()
+    with pytest.raises(IOError):
+        await sync_or_async(circuit_failure())
 
     assert CircuitBreakerMonitor.all_closed() is False
     assert len(list(CircuitBreakerMonitor.get_circuits())) == 6
@@ -66,28 +34,28 @@ def test_circuitbreaker_monitor():
     assert len(list(CircuitBreakerMonitor.get_open())) == 1
 
 
-@patch('test_functional.pseudo_remote_call', return_value=True)
-def test_threshold_hit_prevents_consequent_calls(mock_remote):
-    # type: (Mock) -> None
-    mock_remote.side_effect = IOError('Connection refused')
+async def test_threshold_hit_prevents_consequent_calls(
+    sync_or_async, mock_remote_call, circuit_threshold_1
+):
+    mock_remote_call.side_effect = IOError('Connection refused')
     circuitbreaker = CircuitBreakerMonitor.get('threshold_1')
 
     assert circuitbreaker.closed
 
-    with raises(IOError):
-        circuit_threshold_1()
+    with pytest.raises(IOError):
+        await sync_or_async(circuit_threshold_1())
 
     assert circuitbreaker.opened
 
-    with raises(CircuitBreakerError):
-        circuit_threshold_1()
+    with pytest.raises(CircuitBreakerError):
+        await sync_or_async(circuit_threshold_1())
 
-    mock_remote.assert_called_once_with()
+    mock_remote_call.assert_called_once_with()
 
 
-@patch('test_functional.pseudo_remote_call', return_value=True)
-def test_circuitbreaker_recover_half_open(mock_remote):
-    # type: (Mock) -> None
+async def test_circuitbreaker_recover_half_open(
+    is_async, sync_or_async, mock_remote_call, circuit_threshold_3_timeout_1
+):
     circuitbreaker = CircuitBreakerMonitor.get('threshold_3')
 
     # initial state: closed
@@ -95,26 +63,28 @@ def test_circuitbreaker_recover_half_open(mock_remote):
     assert circuitbreaker.state == STATE_CLOSED
 
     # no exception -> success
-    assert circuit_threshold_3_timeout_1()
+    mock_remote_call.return_value = True
+    assert await sync_or_async(circuit_threshold_3_timeout_1())
 
     # from now all subsequent calls will fail
-    mock_remote.side_effect = IOError('Connection refused')
+    mock_remote_call.side_effect = IOError('Connection refused')
 
     # 1. failed call -> original exception
-    with raises(IOError):
-        circuit_threshold_3_timeout_1()
+    with pytest.raises(IOError):
+        assert await sync_or_async(circuit_threshold_3_timeout_1())
+
     assert circuitbreaker.closed
     assert circuitbreaker.failure_count == 1
 
     # 2. failed call -> original exception
-    with raises(IOError):
-        circuit_threshold_3_timeout_1()
+    with pytest.raises(IOError):
+        assert await sync_or_async(circuit_threshold_3_timeout_1())
     assert circuitbreaker.closed
     assert circuitbreaker.failure_count == 2
 
     # 3. failed call -> original exception
-    with raises(IOError):
-        circuit_threshold_3_timeout_1()
+    with pytest.raises(IOError):
+        assert await sync_or_async(circuit_threshold_3_timeout_1())
 
     # Circuit breaker opens, threshold has been reached
     assert circuitbreaker.opened
@@ -123,21 +93,24 @@ def test_circuitbreaker_recover_half_open(mock_remote):
     assert 0 < circuitbreaker.open_remaining <= 1
 
     # 4. failed call -> not passed to function -> CircuitBreakerError
-    with raises(CircuitBreakerError):
-        circuit_threshold_3_timeout_1()
+    with pytest.raises(CircuitBreakerError):
+        assert await sync_or_async(circuit_threshold_3_timeout_1())
     assert circuitbreaker.opened
     assert circuitbreaker.failure_count == 3
     assert 0 < circuitbreaker.open_remaining <= 1
 
     # 5. failed call -> not passed to function -> CircuitBreakerError
-    with raises(CircuitBreakerError):
-        circuit_threshold_3_timeout_1()
+    with pytest.raises(CircuitBreakerError):
+        assert await sync_or_async(circuit_threshold_3_timeout_1())
     assert circuitbreaker.opened
     assert circuitbreaker.failure_count == 3
     assert 0 < circuitbreaker.open_remaining <= 1
 
     # wait for 1 second (recover timeout)
-    sleep(1)
+    if is_async:
+        await asyncio.sleep(1)
+    else:
+        time.sleep(1)
 
     # circuit half-open -> next call will be passed through
     assert not circuitbreaker.closed
@@ -145,20 +118,20 @@ def test_circuitbreaker_recover_half_open(mock_remote):
     assert circuitbreaker.state == STATE_HALF_OPEN
 
     # State half-open -> function is executed -> original exception
-    with raises(IOError):
-        circuit_threshold_3_timeout_1()
+    with pytest.raises(IOError):
+        assert await sync_or_async(circuit_threshold_3_timeout_1())
     assert circuitbreaker.opened
     assert circuitbreaker.failure_count == 4
     assert 0 < circuitbreaker.open_remaining <= 1
 
     # State open > not passed to function -> CircuitBreakerError
-    with raises(CircuitBreakerError):
-        circuit_threshold_3_timeout_1()
+    with pytest.raises(CircuitBreakerError):
+        assert await sync_or_async(circuit_threshold_3_timeout_1())
 
 
-@patch('test_functional.pseudo_remote_call', return_value=True)
-def test_circuitbreaker_reopens_after_successful_calls(mock_remote):
-    # type: (Mock) -> None
+async def test_circuitbreaker_reopens_after_successful_calls(
+    is_async, sync_or_async, mock_remote_call, circuit_threshold_2_timeout_1
+):
     circuitbreaker = CircuitBreakerMonitor.get('threshold_2')
 
     assert str(circuitbreaker) == 'threshold_2'
@@ -169,20 +142,20 @@ def test_circuitbreaker_reopens_after_successful_calls(mock_remote):
     assert circuitbreaker.failure_count == 0
 
     # successful call -> no exception
-    assert circuit_threshold_2_timeout_1()
+    assert await sync_or_async(circuit_threshold_2_timeout_1())
 
     # from now all subsequent calls will fail
-    mock_remote.side_effect = IOError('Connection refused')
+    mock_remote_call.side_effect = IOError('Connection refused')
 
     # 1. failed call -> original exception
-    with raises(IOError):
-        circuit_threshold_2_timeout_1()
+    with pytest.raises(IOError):
+        await sync_or_async(circuit_threshold_2_timeout_1())
     assert circuitbreaker.closed
     assert circuitbreaker.failure_count == 1
 
     # 2. failed call -> original exception
-    with raises(IOError):
-        circuit_threshold_2_timeout_1()
+    with pytest.raises(IOError):
+        await sync_or_async(circuit_threshold_2_timeout_1())
 
     # Circuit breaker opens, threshold has been reached
     assert circuitbreaker.opened
@@ -191,25 +164,28 @@ def test_circuitbreaker_reopens_after_successful_calls(mock_remote):
     assert 0 < circuitbreaker.open_remaining <= 1
 
     # 4. failed call -> not passed to function -> CircuitBreakerError
-    with raises(CircuitBreakerError):
-        circuit_threshold_2_timeout_1()
+    with pytest.raises(CircuitBreakerError):
+        await sync_or_async(circuit_threshold_2_timeout_1())
     assert circuitbreaker.opened
     assert circuitbreaker.failure_count == 2
     assert 0 < circuitbreaker.open_remaining <= 1
 
     # from now all subsequent calls will succeed
-    mock_remote.side_effect = None
+    mock_remote_call.side_effect = None
 
     # but recover timeout has not been reached -> still open
     # 5. failed call -> not passed to function -> CircuitBreakerError
-    with raises(CircuitBreakerError):
-        circuit_threshold_2_timeout_1()
+    with pytest.raises(CircuitBreakerError):
+        await sync_or_async(circuit_threshold_2_timeout_1())
     assert circuitbreaker.opened
     assert circuitbreaker.failure_count == 2
     assert 0 < circuitbreaker.open_remaining <= 1
 
     # wait for 1 second (recover timeout)
-    sleep(1)
+    if is_async:
+        await asyncio.sleep(1)
+    else:
+        time.sleep(1)
 
     # circuit half-open -> next call will be passed through
     assert not circuitbreaker.closed
@@ -218,7 +194,7 @@ def test_circuitbreaker_reopens_after_successful_calls(mock_remote):
     assert circuitbreaker.state == STATE_HALF_OPEN
 
     # successful call
-    assert circuit_threshold_2_timeout_1()
+    await sync_or_async(circuit_threshold_2_timeout_1())
 
     # circuit closed and reset'ed
     assert circuitbreaker.closed
@@ -226,23 +202,29 @@ def test_circuitbreaker_reopens_after_successful_calls(mock_remote):
     assert circuitbreaker.failure_count == 0
 
     # some another successful calls
-    assert circuit_threshold_2_timeout_1()
-    assert circuit_threshold_2_timeout_1()
-    assert circuit_threshold_2_timeout_1()
+    await sync_or_async(circuit_threshold_2_timeout_1())
+    await sync_or_async(circuit_threshold_2_timeout_1())
+    await sync_or_async(circuit_threshold_2_timeout_1())
 
 
-@patch("test_functional.pseudo_remote_call", return_value=True)
-def test_circuitbreaker_handles_generator_functions(mock_remote):
-    # type: (Mock) -> None
+async def test_circuitbreaker_handles_generator_functions(
+    is_async, mock_remote_call, circuit_generator_failure
+):
     circuitbreaker = CircuitBreakerMonitor.get("circuit_generator_failure")
     assert circuitbreaker.closed
 
-    with raises(IOError):
-        list(circuit_generator_failure())
+    with pytest.raises(IOError):
+        if is_async:
+            [_ async for _ in circuit_generator_failure()]
+        else:
+            list(circuit_generator_failure())
 
     assert circuitbreaker.opened
 
-    with raises(CircuitBreakerError):
-        list(circuit_generator_failure())
+    with pytest.raises(CircuitBreakerError):
+        if is_async:
+            [_ async for _ in circuit_generator_failure()]
+        else:
+            list(circuit_generator_failure())
 
-    mock_remote.assert_called_once_with()
+    mock_remote_call.assert_called_once_with()
